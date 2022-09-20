3dd85dd3d247ad5cef1dbc958f33b13cdc6cd320
glafver <glafver22@gmail.com>
2022-09-20 20:50:15 +0200

    Added firebase. Added routes (need to create pages for them). Changed favicon

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@ import { Routes, Route } from 'react-router-dom'
 import Navigation from './components/Navigation'
 import HomePage from './pages/HomePage'
 import NotFound from './pages/NotFound'
+// import LoginPage from './pages/LoginPage'
+// import LogoutPage from './pages/LogoutPage'
+// import SignupPage from './pages/SignupPage'
+// import ForgotPasswordPage from './pages/ForgotPasswordPage'
+// import UpdateProfilePage from './pages/UpdateProfilePage'
+// import RequireAuth from './components/RequireAuth'
 import './assets/scss/App.scss'
 
 function App() {
@@ -12,9 +18,21 @@ function App() {
 			<Routes>
 				<Route path="/" element={<HomePage />} />
 				<Route path="*" element={<NotFound />} />
+
+				{/* <Route path="/signup" element={<SignupPage />} />
+				<Route path="/login" element={<LoginPage />} />
+				<Route path="/logout" element={<LogoutPage />} />
+				<Route path="/forgot-password" element={<ForgotPasswordPage />} /> */}
+
+				{/* <Route path="/update-profile" element={
+					<RequireAuth>
+						<UpdateProfilePage />
+					</RequireAuth>
+				} /> */}
 			</Routes>
 		</div>
 	)
 }
 
 export default App
+
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -7,13 +7,13 @@ const Navigation = () => {
 	return (
 		<Navbar bg="dark" variant="dark" expand="md">
 			<Container>
-				<Navbar.Brand as={Link} to="/">React Template</Navbar.Brand>
+				<Navbar.Brand as={Link} to="/">Tasty Malmö</Navbar.Brand>
 
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 				<Navbar.Collapse id="basic-navbar-nav">
-					<Nav className="ms-auto">
+					{/* <Nav className="ms-auto">
 						<Nav.Link as={NavLink} end to="/">Home</Nav.Link>
-					</Nav>
+					</Nav> */}
 				</Navbar.Collapse>
 			</Container>
 		</Navbar>
