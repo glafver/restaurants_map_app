67c7f85988a288116669acb60036398ea912ad80
alex <kotzspam@gmail.com>
2022-09-21 14:40:29 +0200

    Simple form for suggesting new Restaurant

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { Routes, Route } from 'react-router-dom'
-import Navigation from './components/Navigation'
-import HomePage from './pages/HomePage'
-import NotFound from './pages/NotFound'
-import './assets/scss/App.scss'
+import { Routes, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import HomePage from "./pages/HomePage";
+import NotFound from "./pages/NotFound";
+import "./assets/scss/App.scss";
 
 function App() {
 	return (
@@ -12,9 +12,10 @@ function App() {
 			<Routes>
 				<Route path="/" element={<HomePage />} />
 				<Route path="*" element={<NotFound />} />
+				<Route path="/suggest" element={<SuggestNewRestaurantPage />} />
 			</Routes>
 		</div>
-	)
+	);
 }
 
-export default App
+export default App;
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,23 +1,30 @@
-import Container from 'react-bootstrap/Container'
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
-import { Link, NavLink } from 'react-router-dom'
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { Link, NavLink } from "react-router-dom";
 
 const Navigation = () => {
 	return (
 		<Navbar bg="dark" variant="dark" expand="md">
 			<Container>
-				<Navbar.Brand as={Link} to="/">React Template</Navbar.Brand>
+				<Navbar.Brand as={Link} to="/">
+					React Template
+				</Navbar.Brand>
 
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 				<Navbar.Collapse id="basic-navbar-nav">
 					<Nav className="ms-auto">
-						<Nav.Link as={NavLink} end to="/">Home</Nav.Link>
+						<Nav.Link as={NavLink} end to="/">
+							Home
+						</Nav.Link>
+						<Nav.Link as={NavLink} end to="/suggest">
+							Suggest
+						</Nav.Link>
 					</Nav>
 				</Navbar.Collapse>
 			</Container>
 		</Navbar>
-	)
-}
+	);
+};
 
-export default Navigation
+export default Navigation;
