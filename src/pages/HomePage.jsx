fc64f5332f0aad2c5334650084240ca218a99130
Wiktoria Dobrzewinska <23wiktoria08@gmail.com>
2022-09-21 10:31:35 +0200

    added google map to homepage with location in Malmp

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,10 @@
 import Container from 'react-bootstrap/Container'
+import Map from '../components/Map'
 
 const HomePage = () => {
 	return (
 		<Container className="py-3">
-			<h1>Welcome!</h1>
+			<Map/>
 		</Container>
 	)
 }
