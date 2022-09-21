521d7495cdf9748c5321c500b77b4c39242b49a7
Wiktoria Dobrzewinska <23wiktoria08@gmail.com>
2022-09-21 22:46:43 +0200

    added blue marker to show exact geolocation of your place

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from 'react'
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 import usePosition from '../hooks/usePosition'
+import MarkerIcon from '../assets/icons/marker.png'
 
 
 const Map = () => {
   const [currentPosition, setCurrentPosition] = useState({ lat: 55.606, lng:  13.021 });
   const [currentZoom, setCurrentZoom] = useState(2);
   const position = usePosition();
+  const [marker, setMarker] = useState(false);
   const api_key = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
-
 
   const containerStyle = {
     width: '100%',
@@ -19,9 +20,10 @@ const Map = () => {
     console.log(position);
     if (position.latitude && position.longitude && !position.error) {
       setCurrentPosition({ lat: position.latitude, lng: position.longitude });
-      setCurrentZoom(13);
+      setCurrentZoom(14);
+      setMarker(true)
     }
-  }, [position.latitude, position.longitude, position.error]);
+  }, [position.latitude, position.longitude, position.error, marker]);
 
   return (
     <LoadScript
@@ -32,9 +34,11 @@ const Map = () => {
         defaultCenter={{ lat: 55.606, lng: 13.021 }}
         center={currentPosition}
         zoom={currentZoom}
-      >
+      >       
+     {marker && <Marker position={currentPosition} icon={MarkerIcon}/> }
+      
         { /* Child components, such as markers, info windows, etc. */ }
-        <></>
+        
       </GoogleMap>
     </LoadScript>
   )
