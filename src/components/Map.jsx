6a01e39243436f96d40ca8f9923f26f77b188c67
Wiktoria Dobrzewinska <23wiktoria08@gmail.com>
2022-09-22 16:49:27 +0200

    updated kod in Map.jsx

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,45 +1,86 @@
 import React, { useState, useEffect } from 'react'
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import { GoogleMap, useJsApiLoader, Marker, StandaloneSearchBox } from '@react-google-maps/api';
 import usePosition from '../hooks/usePosition'
 import MarkerIcon from '../assets/icons/marker.png'
+import LoadingSpinner from '../components/LoadingSpinner'
+
 
 const Map = () => {
-  const [currentPosition, setCurrentPosition] = useState({ lat: 55.606, lng: 13.021 });
+  const [currentPosition, setCurrentPosition] = useState();
   const [currentZoom, setCurrentZoom] = useState(2);
   const position = usePosition();
   const [marker, setMarker] = useState(false);
-  const api_key = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+  const [ libraries ] = useState(['places']);
+  const [searchBox, setSearchBox] = useState(null);
+
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
+    libraries,
+  });
 
   const containerStyle = {
     width: '100%',
     height: '500px'
   };
 
+  const inputStyle = {
+    boxSizing: `border-box`,
+    border: `1px solid transparent`,
+    width: `240px`,
+    height: `32px`,
+    padding: `0 12px`,
+    borderRadius: `3px`,
+    boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+    fontSize: `14px`,
+    outline: `none`,
+    textOverflow: `ellipses`,
+    position: 'absolute',
+    top: '10px',
+    right: '10px',
+  }
+  const onLoad = ref => setSearchBox(ref);
+
+  const onPlacesChanged = () => {
+    console.log('places', searchBox.getPlaces())
+    const places = searchBox.getPlaces();
+    places.forEach(place => {
+      setCurrentPosition(place.geometry.location)
+    })
+  }
+
+
+
   useEffect(() => {
     console.log(position);
     if (position.latitude && position.longitude && !position.error) {
-      setCurrentPosition({ lat: position.latitude, lng: position.longitude });
-      setCurrentZoom(14);
-      setMarker(true)
+        setCurrentPosition({ lat: position.latitude, lng: position.longitude });
+        setCurrentZoom(14);
+        setMarker(true)
     }
   }, [position.latitude, position.longitude, position.error, marker]);
 
   return (
-    <LoadScript
-      googleMapsApiKey={api_key}
-    >
-      <GoogleMap
+  <>
+      {!isLoaded && <LoadingSpinner/>}
+      {isLoaded && <GoogleMap
         mapContainerStyle={containerStyle}
         defaultCenter={{ lat: 55.606, lng: 13.021 }}
         center={currentPosition}
         zoom={currentZoom}
-      >
-        {marker && <Marker position={currentPosition} icon={MarkerIcon} />}
-
-        { /* Child components, such as markers, info windows, etc. */}
-
-      </GoogleMap>
-    </LoadScript>
+      >       
+     {marker && <Marker position={currentPosition} icon={MarkerIcon}/> }
+ 
+        { /* Child components, such as markers, info windows, etc. */ }
+        <StandaloneSearchBox   onLoad={onLoad} onPlacesChanged={onPlacesChanged}>
+            <input
+              type='text'
+              placeholder='Search location'
+              style={inputStyle}
+            />
+          </StandaloneSearchBox>
+      </GoogleMap>}
+      </>
   )
 }
 
