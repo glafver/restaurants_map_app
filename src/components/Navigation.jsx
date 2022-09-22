7dc57155547501f1519db1e8e5ad7548f3cad7bd
glafver <glafver22@gmail.com>
2022-09-22 10:50:48 +0200

    Changed name and removed .env.example

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -12,7 +12,7 @@ const Navigation = () => {
 	return (
 		<Navbar bg="dark" variant="dark" expand="md">
 			<Container>
-				<Navbar.Brand as={Link} to="/">Tasty Malmö</Navbar.Brand>
+				<Navbar.Brand as={Link} to="/">React Template</Navbar.Brand>
 
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 				<Navbar.Collapse id="basic-navbar-nav">
