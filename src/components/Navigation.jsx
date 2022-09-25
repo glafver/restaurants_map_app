cc3b17bcf84d2289d928a6080ad108dff70245aa
Wiktoria Dobrzewinska <23wiktoria08@gmail.com>
2022-09-25 12:40:21 +0200

    change name for react template

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -13,7 +13,7 @@ const Navigation = () => {
 		<Navbar bg="dark" variant="dark" expand="md">
 			<Container>
 				<Navbar.Brand as={Link} to="/">
-					React Template
+					Tasty Malmö
 				</Navbar.Brand>
 
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
