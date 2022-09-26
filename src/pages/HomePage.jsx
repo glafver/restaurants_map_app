1d922023d9d042ce293a2e6d6fe5ad9ce651fa36
glafver <glafver22@gmail.com>
2022-09-26 17:42:07 +0200

    Created carousel on HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,8 @@
 import Container from 'react-bootstrap/Container'
 import { useAuthContext } from '../contexts/AuthContext'
 import Map from '../components/Map'
+import HomePageCarousel from '../components/HomePageCarousel'
+
 
 const HomePage = () => {
 
@@ -8,8 +10,9 @@ const HomePage = () => {
 
 	return (
 		<Container className="py-3">
-			<h1>Welcome {isAdmin && 'admin'}!</h1>
-			<Map />
+			{/* <h1>Welcome {isAdmin && 'admin'}!</h1> */}
+			{/* <Map /> */}
+			<HomePageCarousel />
 		</Container>
 	)
 }
