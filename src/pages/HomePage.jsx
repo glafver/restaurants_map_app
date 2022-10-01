de70039ac34ca3875bbb903661fd676f312258bd
Wiktoria Dobrzewinska <23wiktoria08@gmail.com>
2022-10-01 17:10:13 +0200

    added style to homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,31 +1,13 @@
-import {Row, Col} from 'react-bootstrap'
-// import { useAuthContext } from '../contexts/AuthContext'
-import Map from '../components/Map'
-import ListRestaurants from '../components/ListRestaurants'
-import { collection, orderBy, query } from 'firebase/firestore'
-import { useFirestoreQueryData } from '@react-query-firebase/firestore'
-import { db } from '../firebase'
+import Logo from '../assets/images/Logo.png'
+import { Link } from 'react-router-dom'
+
 
 const HomePage = () => {
 
-	const queryRef = query(
-		collection(db, 'restaurants'),
-		orderBy('geolocation')
-	)
-  const { data: restaurants, isLoading } = useFirestoreQueryData(['restaurants'], queryRef, {
-		idField: 'id',
-		subscribe: true,
-	})
-
-	// const { isAdmin } = useAuthContext()
-
 	return (
-		<div className="">
-			<Row>
-			{!isLoading && <Col className="pe-0" sm={3}><ListRestaurants restaurants={restaurants}/></Col> }
-			<Col className="ps-0" sm={9}><Map /></Col>
-			</Row>
-			
+		<div className="homepage">
+			<img src={Logo} alt="logo" className='logo'/><br/>
+			<Link className='link-to-map' to={'/map'}>Show restaurants near me</Link>
 		</div>
 	)
 }
