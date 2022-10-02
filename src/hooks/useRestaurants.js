bb777739dc2f878963c5e857306d02f11c802a95
glafver <glafver22@gmail.com>
2022-10-02 13:43:08 +0200

    Commit

--- src/hooks/useRestaurants.js
+++ src/hooks/useRestaurants.js
@@ -13,7 +13,10 @@ const useRestaurants = () => {
 		sort: searchParams.get('sort')
 	}
 
-	let queryRef = query(collection(db, 'restaurants'))
+	const collectionRef = collection(db, 'restaurants')
+
+	const queryRef = query(collectionRef, where('cuisine', '==', params.cuisine), where('approved', '==', true))
+	// let queryRef = query(collection(db, 'restaurants'))
 
 	const restaurantQuery = useFirestoreQueryData(['restaurants'], queryRef, {
 		idField: 'id',
@@ -22,34 +25,40 @@ const useRestaurants = () => {
 
 	if (params.cuisine || params.type || params.sort) {
 
-		if (!restaurantQuery.data) {
-			return
-		}
+		// if (!restaurantQuery.data) {
+		// 	return
+		// }
 
-		let restaurants = restaurantQuery.data
+		// let restaurants = restaurantQuery.data
 
 		if (params.cuisine) {
-			restaurants = restaurants.filter(rest => {
-				return rest.cuisine === params.cuisine
-			})
+			// restaurants = restaurants.filter(rest => {
+			// 	return rest.cuisine === params.cuisine
+			// })
+			queryRef = query(collectionRef, where('cuisine', '==', params.cuisine))
 		}
 
 		if (params.type) {
-			restaurants = restaurants.filter(rest => {
-				return rest.type === params.type
-			})
+			// restaurants = restaurants.filter(rest => {
+			// 	return rest.type === params.type
+			// })
+			queryRef = query(collectionRef, where('type', '==', params.type))
 		}
 
 		if (params.sort) {
 			if (params.sort === 'asc') {
-				restaurants.sort((a, b) => a.name.localeCompare(b.name))
+				// restaurants.sort((a, b) => a.name.localeCompare(b.name))
+				queryRef = query(collectionRef, orderBy('created', 'asc'))
 			}
 			if (params.sort === 'dec') {
-				restaurants.sort((a, b) => b.name.localeCompare(a.name))
+				// restaurants.sort((a, b) => b.name.localeCompare(a.name))
+				queryRef = query(collectionRef, orderBy('created', 'decs'))
 			}
 		}
 
 		return restaurants
+	} else {
+		queryRef = query(collection(db, 'restaurants'))
 	}
 
 	return restaurantQuery.data
