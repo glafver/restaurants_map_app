d641d54eb06e3d9550009febb3c112a602033751
glafver <glafver22@gmail.com>
2022-10-03 23:27:31 +0200

    Test

--- src/components/__test__/EditRestaurantForm.test.jsx
+++ src/components/__test__/EditRestaurantForm.test.jsx
@@ -2,17 +2,30 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import EditRestaurantForm from '../EditRestaurantForm'
 
-it("the table is not empty", () => {
+it("the table name is not empty", () => {
 
 	const restaurant = { name: 'test' }
 
 	render(<EditRestaurantForm restaurant={restaurant} />)
 
 	const inputElement = screen.getByLabelText(/name */i)
-	// const restaurantName = screen.getByRole('textbox', { name: /name/i })
-	console.log(inputElement)
 
 	expect(inputElement.value).not.toBe('')
+})
+
+it("when user tries to leave name field empty it returns the initial value", async () => {
+
+	const restaurant = { name: 'test' }
+
+	render(<EditRestaurantForm restaurant={restaurant} />)
+
+	const inputElement = screen.getByLabelText(/name */i)
+	const btnSubmit = screen.getByRole('button', { name: /submit/i })
+
+	await userEvent.clear(inputElement)
+	await userEvent.click(btnSubmit)
+
+	expect(inputElement.value).toBe('test')
 })
 
 
