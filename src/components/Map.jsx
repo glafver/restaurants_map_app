b961251dd7dbc5132f273796510f56d3abbc6f52
Wiktoria Dobrzewinska <23wiktoria08@gmail.com>
2022-10-06 13:12:24 +0200

    size of page

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -24,7 +24,7 @@ const Map = ({ restaurants }) => {
 
   const containerStyle = {
     width: '100%',
-    height: '625px'
+    height: '620px'
   };
 
 
--- src/components/RestaurantsList.jsx
+++ src/components/RestaurantsList.jsx
@@ -6,7 +6,7 @@ const RestaurantsList = ({ restaurants }) => {
     return (
 
         <>
-            <Row className="overflow-auto" style={{ height : '625px'}}>
+            <Row className="overflow-auto" style={{ height : '620px'}}>
                 {restaurants && restaurants.map((restaurant, index) => (
                     <RestaurantsListCard key={index} restaurant={restaurant}></RestaurantsListCard>
                 ))}
